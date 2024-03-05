2b0119a41cba75a42aa00a5992b200c0d1084f36
LeonLonsdale <leon.coding@icloud.com>
2024-03-05 18:28:22 +0000

    App - Common Footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,11 @@
 const AppFooter = () => {
-  return <footer>AppFooter</footer>;
+  return (
+    <footer className="border-t border-black/5 py-5 mt-auto">
+      <small className="opacity-50">
+        &copy; 2024 ByteGrad. All rights reserved.
+      </small>
+    </footer>
+  );
 };
 
 export default AppFooter;
