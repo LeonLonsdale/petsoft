ac36fe2efe6ce752a86c04353b0c065c9e528abb
LeonLonsdale <leon.coding@icloud.com>
2024-03-10 12:03:08 +0000

    Dashboard - petlist item layout

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
+
 const PetList = () => {
   return (
     <section className="bg-white border-b border-black/[0.08]">
       <ul>
         <li>
-          <button>Benjamin</button>
+          <button className="flex h-[70px] w-full cursor-pointer items-center px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+            <Image
+              src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+              alt="Pet Image"
+              width={45}
+              height={45}
+              className="rounded-full object-cover"
+            />
+            <p className="font-semibold">Benjamin</p>
+          </button>
         </li>
       </ul>
     </section>
