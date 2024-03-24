bd0bd80a05171a498813d8f94572adb9e771d2f8
LeonLonsdale <leon.coding@icloud.com>
2024-03-24 21:37:08 +0000

    Dashboard - Form validation with Zod

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -6,31 +6,63 @@ import { Textarea } from '@/components/ui/textarea';
 import { usePetContext } from '@/contexts/pet-context-provider';
 import PetFormButton from './pet-form-btn';
 import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 type PetFormProps = {
   actionType: 'add' | 'edit';
   onFormSubmission: () => void;
 };
 
-type TPetForm = {
-  name: string;
-  ownerName: string;
-  imageUrl: string;
-  age: number;
-  notes: string;
-};
+const petFormSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, { message: 'Name is required' })
+    .max(100, { message: 'Name must be less than 100 characters' }),
+  ownerName: z
+    .string()
+    .trim()
+    .min(1, { message: 'Owner name is required' })
+    .max(100, { message: 'Owner name must be less than 100 characters' }),
+  imageUrl: z.union([
+    z.literal(''),
+    z.string().trim().url({ message: 'Invalid URL' }),
+  ]),
+  age: z.coerce
+    .number()
+    .int()
+    .positive()
+    .min(1, { message: 'Age must be greater than 0' })
+    .max(999, { message: 'Age must be less than 1000' }),
+  notes: z.union([
+    z.literal(''),
+    z
+      .string()
+      .trim()
+      .max(1000, { message: 'Notes must be less than 500 characters' }),
+  ]),
+});
+
+type TPetForm = z.infer<typeof petFormSchema>;
 
 const PetForm = ({ actionType, onFormSubmission }: PetFormProps) => {
   const { selectedPet, handleAddPet, handleUpdatePet } = usePetContext();
 
   const {
     register,
-    formState: { isSubmitting, errors },
-  } = useForm<TPetForm>();
+    trigger,
+    formState: { errors },
+  } = useForm<TPetForm>({
+    resolver: zodResolver(petFormSchema),
+  });
 
   return (
     <form
       action={async (formData: FormData) => {
+        const result = await trigger();
+        if (!result) return;
+
         onFormSubmission();
 
         const petData = {
@@ -52,34 +84,40 @@ const PetForm = ({ actionType, onFormSubmission }: PetFormProps) => {
       <fieldset className='space-y-3'>
         <div className='space-y-1'>
           <Label htmlFor='name'>Name</Label>
-          <Input id='name' {...register('name', { required: true })} />
+          <Input
+            id='name'
+            {...register('name', {
+              required: 'Name is required',
+              minLength: {
+                value: 3,
+                message: 'Name must be at least 3 characters long',
+              },
+            })}
+          />
           {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
         </div>
         <div className='space-y-1'>
           <Label htmlFor='ownerName'>Owner Name</Label>
-          <Input
-            id='ownerName'
-            {...register('ownerName', { required: true })}
-          />
+          <Input id='ownerName' {...register('ownerName')} />
           {errors.ownerName && (
             <p className='text-red-500'>{errors.ownerName.message}</p>
           )}
         </div>
         <div className='space-y-1'>
           <Label htmlFor='imageUrl'>Image Url</Label>
-          <Input id='imageUrl' {...register('imageUrl', { required: true })} />
+          <Input id='imageUrl' {...register('imageUrl')} />
           {errors.imageUrl && (
             <p className='text-red-500'>{errors.imageUrl.message}</p>
           )}
         </div>
         <div className='space-y-1'>
           <Label htmlFor='age'>Age</Label>
-          <Input id='age' {...register('age', { required: true })} />
+          <Input id='age' {...register('age')} />
           {errors.age && <p className='text-red-500'>{errors.age.message}</p>}
         </div>
         <div className='space-y-1'>
           <Label htmlFor='notes'>Notes</Label>
-          <Textarea id='notes' {...register('notes', { required: true })} />
+          <Textarea id='notes' {...register('notes')} />
           {errors.notes && (
             <p className='text-red-500'>{errors.notes.message}</p>
           )}
