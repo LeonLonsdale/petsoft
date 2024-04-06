8aa4f87b2e75887f41478aae31b716b0c7b10583
LeonLonsdale <leon.coding@icloud.com>
2024-04-06 19:56:29 +0100

    Auth - zod validation registration

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -3,20 +3,25 @@
 import bcrypt from 'bcryptjs';
 import prisma from '@/lib/db';
 import { signIn } from '@/lib/auth';
+import { authSchema } from '@/lib/validations';
 
-export const register = async (formData: FormData) => {
-  const user = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
-
-  const response = await prisma.user.create({
+export const register = async (formData: unknown) => {
+  // check type of formData is FormData
+  // convert to object so it can be parsed
+  // parse with zod
+  if (!(formData instanceof FormData)) return { message: 'Invalid credentials' };
+  const formDataObject = Object.fromEntries(formData.entries());
+  const validatedFormData = authSchema.safeParse(formDataObject);
+  if (!validatedFormData.success) return { message: 'Invalid credentials' }
+ 
+  const { email, password } = validatedFormData.data;
+  await prisma.user.create({
     data: {
-      email: user.email,
-      hashedPassword: await bcrypt.hash(user.password, 10),
+      email,
+      hashedPassword: await bcrypt.hash(password, 10),
     },
   });
 
   // sign in and create token
-  await signIn('credentials', user);
+  await signIn('credentials', {email, password});
 };
