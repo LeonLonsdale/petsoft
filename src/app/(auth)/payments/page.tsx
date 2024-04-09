c07d830972e3e480102619f309a7f2ba53546166
LeonLonsdale <leon.coding@icloud.com>
2024-04-09 15:15:59 +0100

    Payments - Handle return from stripe

--- src/app/(auth)/payments/page.tsx
+++ src/app/(auth)/payments/page.tsx
@@ -1,19 +1,45 @@
+// 'use client';
+
 import H1 from '@/components/h1';
-import { Button } from '@/components/ui/button';
-import * as actions from '@/actions';
 import MakePaymentButton from '@/components/make-payment-button';
+// import { useEffect } from 'react';
+// import { toast } from 'sonner';
 
 type PaymentsPageProps = {
-  params: string;
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-const PaymentsPage = ({ params }: PaymentsPageProps) => {
+const PaymentsPage = ({ searchParams }: PaymentsPageProps) => {
+  // useEffect(() => {
+  //   if (searchParams.success) {
+  //     setTimeout(() => {
+  //       toast.success('Successful');
+  //     }, 1000);
+  //   }
+  // }, [searchParams.success]);
+
   return (
     <main className='flex flex-col items-center space-y-10'>
-      <H1>Petsoft access requires payment</H1>
-      <MakePaymentButton />
+      {!searchParams.success && !searchParams.cancelled && (
+        <H1>Petsoft access requires payment</H1>
+      )}
+      {searchParams.success && (
+        <>
+          <H1 className='text-green-700'>Your payment was successful</H1>
+          <p>You now have lifetime access</p>
+        </>
+      )}
+      {searchParams.cancelled && (
+        <>
+          <H1 className='text-red-700'>You cancelled your payment</H1>
+          <p className='mx-2 text-center'>
+            We're very sorry you've decided not to join us. You can start again
+            whenever you're ready.
+          </p>
+        </>
+      )}
+      {!searchParams.success && <MakePaymentButton />}
     </main>
   );
 };
-
 export default PaymentsPage;
--- src/components/make-payment-button.tsx
+++ src/components/make-payment-button.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import * as actions from '@/actions';
+import { useTransition } from 'react';
 
 const MakePaymentButton = () => {
+  const [isPending, startTransition] = useTransition();
   return (
     <Button
       onClick={async () => {
-        await actions.createCheckoutSession();
+        startTransition(async () => {
+          await actions.createCheckoutSession();
+        });
       }}
+      disabled={isPending}
     >
       Buy lifetime access for $299
     </Button>
