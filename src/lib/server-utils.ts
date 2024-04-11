58f48180c9635de73ddefd12e11417f69a8ba81e
LeonLonsdale <leon.coding@icloud.com>
2024-04-11 23:44:47 +0100

    Fix - fix imports - auth.ts renamed

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -1,41 +1,41 @@
 import 'server-only';
-import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
-import { paths } from "./paths";
+import { auth } from '@/lib/auth-no-edge';
+import { redirect } from 'next/navigation';
+import { paths } from './paths';
 import { Pet, User } from '@prisma/client';
 import prisma from './db';
 
 export const authCheck = async () => {
   const session = await auth();
 
-  if (!session?.user) redirect(paths.login.path())
+  if (!session?.user) redirect(paths.login.path());
 
-  return session
-}
+  return session;
+};
 
 export const getPetByPetId = async (petId: Pet['id']) => {
   const pet = await prisma.pet.findUnique({
     where: {
       id: petId,
-    }
-  })
+    },
+  });
 
   return pet;
-}
+};
 
 export const getPetsByUserId = async (userId: User['id']) => {
   const pets = await prisma.pet.findMany({
-    where: { userId }
-  })
+    where: { userId },
+  });
 
   return pets;
-}
+};
 
 export const getUserByEmail = async (email: User['email']) => {
   const user = await prisma.user.findUnique({
     where: {
       email,
-    }
-  })
+    },
+  });
   return user;
-}
+};
